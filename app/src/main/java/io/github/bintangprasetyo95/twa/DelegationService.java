cf634ab61dd6aa57cd3ff1ce191062c95f79a418
bintangprasetyo95 <sutpidotdash@gmail.com>
2025-11-30 00:07:59 +0700

    APK Update (BubbleWrap)

---
+++ app/src/main/java/io/github/bintangprasetyo95/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.bintangprasetyo95.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
